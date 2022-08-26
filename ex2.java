e1f6a84bea087a346567eb9520f411cc249c6b55
nicas20 <100000527+nicas20@users.noreply.github.com>
2022-08-25 22:10:43 -0300

    ex2

---
+++ ex2.java
@@ -0,0 +1,20 @@
+package com.mycompany.prova1;
+
+import java.util.Scanner;
+
+public class ex2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double aumento = 1.5, salario = 1000, ano = 1996;
+
+        do {
+            salario = salario + (salario * aumento / 100);
+            aumento = aumento * 2;
+            ano++;
+        } while (ano <= 2022);
+
+        System.out.println(salario);
+    }
+
+}
